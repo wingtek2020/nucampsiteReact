659e60fd064066319375cdcc9e438bd06525b6b2
Christine Wingfield <wingtek2020@gmail.com>
2024-01-06 14:14:24 -0500

    starting workshop

--- screens/CampsiteInfoScreen.js
+++ screens/CampsiteInfoScreen.js
@@ -1,20 +1,12 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { toggleFavorite } from '../features/favorites/favoritesSlice';
-import {
-    FlatList, StyleSheet, Text,
-    Button, Modal, View
-} from 'react-native';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { useSelector, useDispatch } from 'react-redux';
 import RenderCampsite from '../features/campsites/RenderCampsite';
-import { useState } from 'react';
-
-import {COMMENTS} from '../shared/comments';
+import { toggleFavorite } from '../features/favorites/favoritesSlice';
 
 const CampsiteInfoScreen = ({ route }) => {
     const { campsite } = route.params;
-
-    const [comments, setComments] = useState(COMMENTS);
-    const [favorite, setFavorite] = useState(false);
-
+    const comments = useSelector((state) => state.comments);
+    const favorites = useSelector((state) => state.favorites);
     const dispatch = useDispatch();
 
     const renderCommentItem = ({ item }) => {
@@ -31,7 +23,7 @@ const CampsiteInfoScreen = ({ route }) => {
 
     return (
         <FlatList
-            data={comments.filter(
+            data={comments.commentsArray.filter(
                 (comment) => comment.campsiteId === campsite.id
             )}
             renderItem={renderCommentItem}
@@ -44,8 +36,8 @@ const CampsiteInfoScreen = ({ route }) => {
                 <>
                     <RenderCampsite
                         campsite={campsite}
-                        isFavorite={favorite}
-                        markFavorite={() => setFavorite(true)}
+                        isFavorite={favorites.includes(campsite.id)}
+                        markFavorite={() => dispatch(toggleFavorite(campsite.id))}
                     />
                     <Text style={styles.commentsTitle}>Comments</Text>
                 </>
@@ -68,11 +60,6 @@ const styles = StyleSheet.create({
         paddingVertical: 10,
         paddingHorizontal: 20,
         backgroundColor: '#fff'
-    },
-    cardContainer: {
-        padding: 0,
-        margin: 0,
-        marginBottom: 20
     }
 });
 
