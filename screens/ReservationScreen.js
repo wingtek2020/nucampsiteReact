009c4763247f4961c3d2823db6f3bffbfba64ad7
Christine Wingfield <wingtek2020@gmail.com>
2024-01-14 16:12:10 -0500

    before workshop4

--- screens/ReservationScreen.js
+++ screens/ReservationScreen.js
@@ -5,13 +5,13 @@ import {
     ScrollView,
     StyleSheet,
     Switch,
-    Button, Alert
+    Button,
+    Alert
 } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
 import DateTimePicker from '@react-native-community/datetimepicker';
-
 import * as Animatable from 'react-native-animatable';
-
+import * as Notifications from 'expo-notifications';
 
 const ReservationScreen = () => {
     const [campers, setCampers] = useState(1);
@@ -26,27 +26,36 @@ const ReservationScreen = () => {
     };
 
     const handleReservation = () => {
+        const message = `Number of Campers: ${campers}
+                            \nHike-In? ${hikeIn}
+                            \nDate: ${date.toLocaleDateString('en-US')}`;
         Alert.alert(
             'Begin Search?',
-            'Number of Campers: ' + campers + '\r' +
-            'Hike-In? ' + hikeIn + '\r' +
-            'Date: ' + date.toLocaleDateString('en-us', { year:"2-digit", month:"numeric", day:"numeric"})
-            ,
+            message,
             [
                 {
                     text: 'Cancel',
-                    onPress: () =>
-                        resetForm(),
+                    onPress: () => {
+                        console.log('Reservation Search Canceled');
+                        resetForm();
+                    },
                     style: 'cancel'
                 },
                 {
                     text: 'OK',
-                    onPress: () =>
-                        resetForm()
+                    onPress: () => {
+                        presentLocalNotification(
+                            date.toLocaleDateString('en-US')
+                        );
+                        resetForm();
+                    }
                 }
             ],
             { cancelable: false }
-        )
+        );
+        console.log('campers:', campers);
+        console.log('hikeIn:', hikeIn);
+        console.log('date:', date);
     };
 
     const resetForm = () => {
@@ -56,15 +65,37 @@ const ReservationScreen = () => {
         setShowCalendar(false);
     };
 
-    
+    const presentLocalNotification = async (reservationDate) => {
+        const sendNotification = () => {
+            Notifications.setNotificationHandler({
+                handleNotification: async () => ({
+                    shouldShowAlert: true,
+                    shouldPlaySound: true,
+                    shouldSetBadge: true
+                })
+            });
+
+            Notifications.scheduleNotificationAsync({
+                content: {
+                    title: 'Your Campsite Reservation Search',
+                    body: `Search for ${reservationDate} requested`
+                },
+                trigger: null
+            });
+        };
+
+        let permissions = await Notifications.getPermissionsAsync();
+        if (!permissions.granted) {
+            permissions = await Notifications.requestPermissionsAsync();
+        }
+        if (permissions.granted) {
+            sendNotification();
+        }
+    };
 
     return (
         <ScrollView>
-            <Animatable.View
-                animation='zoomIn'
-                duration={2000}
-                delay={1000}
-            >
+            <Animatable.View animation='zoomIn' duration={2000} delay={1000}>
                 <View style={styles.formRow}>
                     <Text style={styles.formLabel}>Number of Campers:</Text>
                     <Picker
@@ -115,15 +146,15 @@ const ReservationScreen = () => {
                         accessibilityLabel='Tap me to search for available campsites to reserve'
                     />
                 </View>
-            </Animatable.View>            
+            </Animatable.View>
         </ScrollView>
     );
 };
 
 const styles = StyleSheet.create({
     formRow: {
-        alignItems: 'left',
-        justifyContent: 'left',
+        alignItems: 'center',
+        justifyContent: 'center',
         flex: 1,
         flexDirection: 'row',
         margin: 20
@@ -134,9 +165,6 @@ const styles = StyleSheet.create({
     },
     formItem: {
         flex: 1
-    },
-    alertStyle: {
-        textAlign: 'left'
     }
 });
 
